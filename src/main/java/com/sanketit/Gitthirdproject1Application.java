b36712f03624e0bf05dc2c1b7913216dfb267802
sanket07 <sanketpadole218@gmail.com>
2022-08-04 11:41:10 +0530

    creted sucess

---
+++ src/main/java/com/sanketit/Gitthirdproject1Application.java
@@ -0,0 +1,13 @@
+package com.sanketit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitthirdproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitthirdproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanketit/Gitthirdproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanketit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitthirdproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
